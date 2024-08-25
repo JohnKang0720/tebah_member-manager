72fa377715b6f2fc8fdeb44b2e3a8ea08ab87888
Sebin-Leeee <sebin.lee.work@gmail.com>
2024-08-25 14:43:44 -0700

    stylize youth & secondry page

--- search/frontend/src/components/Second.jsx
+++ search/frontend/src/components/Second.jsx
@@ -1,37 +1,71 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { useFetch } from '../useFetch'
-import DeleteMember from './Util/DeleteMember';
-import View from './Util/View';
-import EditMember from './Util/EditMember';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useFetch } from "../useFetch";
+import DeleteMember from "./Util/DeleteMember";
+import View from "./Util/View";
+import EditMember from "./Util/EditMember";
 
 function Second() {
   const [text, setText] = useState("");
   const [filtered, setFiltered] = useState([]);
 
-  const [data, fields, error, loading] = useFetch("main/secondary", ["id", "korean", "english_name", "mobile", "suite", "street"]);
+  const [data, fields, error, loading] = useFetch("main/secondary", [
+    "id",
+    "korean",
+    "english_name",
+    "mobile",
+    "suite",
+    "street",
+  ]);
 
   useEffect(() => {
     if (data) {
-      let filteredArray = data.filter(info => info.korean.includes(text) || info.english_name.toLowerCase().includes(text.toLowerCase()));
-      setFiltered(filteredArray)
+      let filteredArray = data.filter(
+        (info) =>
+          info.korean.includes(text) ||
+          info.english_name.toLowerCase().includes(text.toLowerCase())
+      );
+      setFiltered(filteredArray);
     }
-  }, [text])
+  }, [text]);
 
   return (
-    <div><h1> 청년부 데이터 </h1>
-    <br />
-      <div class="input-div">
-        <div class="inputs">
-          <h5> 맴버 검색: </h5>
+    <div
+      style={{ paddingTop: "30px", paddingBottom: "30px", textAlign: "center" }}
+    >
+      <h1> 청년부 데이터 </h1>
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        class="input-div"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          class="inputs"
+        >
+          <h5 style={{ width: "100%", paddingRight: "20px" }}> 맴버 검색: </h5>
           <br />
-          <input class="form-control" placeholder='검색' onChange={e => setText(e.target.value)} />
-          <br /> 
+          <input
+            style={{ padding: "20px", width: "200px" }}
+            class="form-control"
+            placeholder="검색"
+            onChange={(e) => setText(e.target.value)}
+          />
+          <br />
         </div>
       </div>
       <View data={[loading, text, data, filtered, fields, fields.length]} />
     </div>
-  )
+  );
 }
 
-export default Second
+export default Second;
--- search/frontend/src/components/Youth.jsx
+++ search/frontend/src/components/Youth.jsx
@@ -20,13 +20,13 @@ function Youth() {
   }, [text])
 
   return (
-    <div><h1> 유스 데이터 </h1>
+    <div style={{ paddingTop: '30px', paddingBottom: '30px', textAlign: 'center' }}>
+    <h1> 유스 데이터 </h1>
       <br />
-      <div class="input-div">
-        <div class="inputs">
-          <h5> 맴버 검색: </h5>
-          <br />
-          <input class="form-control" placeholder='검색' onChange={e => setText(e.target.value)} />
+      <div style={{ display:'flex', justifyContent:'center', alignItems:'center'}} class="input-div">
+        <div style={{ display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+          <h5 style={{width:'100%', paddingRight:'20px'}}> 맴버 검색: </h5>
+          <input style={{padding:'20px', width:'200px'}} class="form-control" placeholder='검색' onChange={e => setText(e.target.value)} />
         </div>
       </div>
       <br />
