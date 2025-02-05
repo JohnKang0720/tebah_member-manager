2775df439fa19cd5d9f006f7f01de2d2fa5306a8
John Kang <76849492+JohnKang0720@users.noreply.github.com>
2025-02-04 22:28:28 -0800

    Update db.js

--- search/backend/db.js
+++ search/backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const db = new Pool({
-  connectionString: "postgres://default:LgnO1f8UPHDI@ep-crimson-paper-a45txdup-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+  connectionString: "postgresql://default:LgnO1f8UPHDI@ep-crimson-paper-a45txdup-pooler.us-east-1.aws.neon.tech/tebah_db?sslmode=require",
 })
 
 module.exports = db
